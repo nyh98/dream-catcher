06863dfae03e57b32b28f8620e893d0e9cc033ba
nyh <nsi3358@naver.com>
2024-10-22 23:21:37 +0900

    diary CRUD 틀 생성

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -1,11 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Delete, Patch, Get } from '@nestjs/common';
 import { DiaryService } from './diary.service';
 import { CreateDiaryDto } from './dto/create-diary.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('diary')
 @Controller('diarys')
 export class DiaryController {
   constructor(private readonly diaryService: DiaryService) {}
 
+  @ApiOperation({ summary: '일기 작성' })
   @Post()
   createDiary(@Body() createDiaryDto: CreateDiaryDto) {}
+
+  @ApiOperation({ summary: '여러 일기 조회' })
+  @Get()
+  getDiarys() {}
+
+  @ApiOperation({ summary: '단일 일기 조회' })
+  @Get('/:diaryid')
+  getDiary() {}
+
+  @ApiOperation({ summary: '일기 수정' })
+  @Patch()
+  updateDiary() {}
+
+  @ApiOperation({ summary: '일기 삭제' })
+  @Delete()
+  deleteDiary() {}
 }
