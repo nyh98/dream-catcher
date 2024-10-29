7f085716f012b198f6499b7e2ec3d658e0c2bb75
nyh <nsi3358@naver.com>
2024-10-29 17:44:43 +0900

    cors origin credentials 추가

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
+  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
   await app.listen(3000);
 }
 bootstrap();
