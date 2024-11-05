2fd70db9920559dd2c4174902914d57656e545c3
nyh <nsi3358@naver.com>
2024-11-05 16:20:03 +0900

    디버깅

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,4 +1,9 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { AuthService } from './auth.service';
 
@@ -10,7 +15,7 @@ export class AuthMiddleware implements NestMiddleware {
     const credentials = req.headers.authorization;
 
     if (!credentials) {
-      return next();
+      throw new BadRequestException('인증 정보가 없습니다');
     }
 
     const [authScheme, token] = credentials.split(' ');
@@ -25,8 +30,11 @@ export class AuthMiddleware implements NestMiddleware {
       }
       //토큰 검증
       const tokenData = await this.authService.validKakaoToken(token);
+      console.log('tokenData', tokenData);
       const user = await this.authService.getKakaoUser(tokenData.id.toString());
+      console.log(user);
       user && (req.user = user);
+      return next();
     }
 
     next();
